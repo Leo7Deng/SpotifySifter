c656f4bcca8690d192ff5c3091fa9ad735d6cd34
Leo Deng <leo7deng@gmail.com>
2023-10-12 00:30:36 -0400

    update prevqueue

--- api/cron.py
+++ api/cron.py
@@ -2,23 +2,24 @@ import atexit
 import os
 import requests
 from apscheduler.schedulers.background import BackgroundScheduler
-from models import db, User, OAuth, Playlist
+from app_init import db
+from models import User, OAuth, Playlist, PrevQueue
 
 QUEUE_ENDPOINT = "https://api.spotify.com/v1/me/player/queue"
 RECENTLY_PLAYED_ENDPOINT = "https://api.spotify.com/v1/me/player/recently-played"
 CURRENTLY_PLAYING_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing"
 EMAIL_ENDPOINT = "https://api.spotify.com/v1/me"
 
+
 def get_response(access_token, endpoint):
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
+    headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(endpoint, headers=headers)
 
     if not response.ok:
-        raise Exception(f'HTTP error! status: {response.status_code}')
-    
+        raise Exception(f"HTTP error! status: {response.status_code}")
+
     return response.json()
+
 
 def set_currently_playing(user_id, playlist_uri):
     user = User.query.get(user_id)
@@ -35,53 +36,77 @@ def set_currently_playing(user_id, playlist_uri):
             playlist.currently_playing = False
 
     db.session.commit()
-
     return "Currently playing updated successfully"
 
-def get_currently_playing_playlist():
+
+def update_currently_playing_playlist():
     for user in User.query.all():
-        user_id = OAuth.query.filter_by(user_id=user.id).first()
-        if user_id:
-            access_token = user_id.access_token
-            print(f"User {user.email} has access token: {access_token}")
-        else:
-            print(f"No OAuth record found for user {user.email}")
-        
-        response = get_response(access_token=access_token, endpoint=CURRENTLY_PLAYING_ENDPOINT)
+        oauth = OAuth.query.filter_by(user_id=user.id).first()
+        access_token = oauth.access_token # pyright: ignore[reportOptionalMemberAccess]
+        print(f"User {user.email} has access token: {access_token}")
+        response = get_response(
+            access_token=access_token, endpoint=CURRENTLY_PLAYING_ENDPOINT
+        )
         uri = response["context"]["uri"]
         set_currently_playing(user_id=user.id, playlist_uri=uri)
-        
 
 
-        
+def get_current_queue(user_id):
+    oauth = OAuth.query.get(user_id)
+    access_token = oauth.access_token # pyright: ignore[reportOptionalMemberAccess]
+    response = get_response(access_token=access_token, endpoint=QUEUE_ENDPOINT)
+    queue = [track["uri"] for track in response["queue"]]
+    print(f"Queue: {queue}")
+    return queue
+
+
+def set_prev_queue(user_id, prev_queue):
+    PrevQueue.query.filter_by(user_id=user_id).delete()
+    for i, uri in enumerate(prev_queue, start=1):
+        prev_queue_item = PrevQueue(track_id=uri, queue_index=i, user_id=user_id) # type: ignore
+        db.session.add(prev_queue_item)
+
+    db.session.commit()
+    print("Prev queue updated successfully")
+
+
+def skip_logic():
+    for user in User.query.all():
+        current_queue = get_current_queue(user_id=user.id)
+        set_prev_queue(user_id=user.id, prev_queue=current_queue)
+
         #     user.currently_listening = True
         # else:
         #     user.currently_listening = False
         # db.session.commit()
 
-def run():
-    new_playlist = Playlist(
-    name="New", 
-    playlist_id="spotify:playlist:0S5MFXu9cG5TKLnLuECDBH",  
-    user_id=1,  
-    currently_playing=False  
-)
-    db.session.add(new_playlist)
-    db.session.commit()
 
-    
-    get_currently_playing_playlist()
+def run():
+    update_currently_playing_playlist()
+    skip_logic()
+
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=get_currently_playing_playlist, args=(), trigger="interval", minutes=5)
+    scheduler.add_job(
+        func=update_currently_playing_playlist, args=(), trigger="interval", minutes=1
+    )
+    scheduler.add_job(func=skip_logic, args=(), trigger="interval", minutes=1)
     # scheduler.add_job(func=skip_logic, args=(), trigger="interval", seconds=5)
     scheduler.start()
     # blueprint.storage = SQLAlchemyStorage(OAuth, db.session, user=current_user)
     # db.session.commit()
     # Shut down the scheduler when exiting the app
-    
+
     atexit.register(lambda: scheduler.shutdown())
 
 
+# new_playlist = Playlist(
+#     name="New",
+#     playlist_id="spotify:playlist:0S5MFXu9cG5TKLnLuECDBH",
+#     user_id=1,
+#     currently_playing=False
+# )
+#     db.session.add(new_playlist)
+#     db.session.commit()
 
 
 # def header(endpoint):
@@ -92,7 +117,7 @@ def run():
 
 # def skip_logic(queue_data, recently_played_data, user_email):
 #     for user in User.query.all():
-        
+
 
 #     user_data = User.query.filter_by(user_email=user_email).first()
 
@@ -105,9 +130,9 @@ def run():
 #         db.session.add(user_data)
 
 #     user_data.prev_queue = track_names
-#     user_data.skipped_once = list(set(skipped_tracks_60)) 
+#     user_data.skipped_once = list(set(skipped_tracks_60))
 #     user_data.skipped_twice = list(set([track for track in user_data.skipped_once if track in skipped_tracks_60]))
-    
+
 #     #recently_played is the recently played tracks that were not skipped
 #     recently_played = [item['track']['name'] for item in recently_played_data['items']]
 
@@ -129,21 +154,21 @@ def run():
 
 #     #after skipped_twice is checked, then skipped_once is updated
 #     user_data.skipped_once.extend(skipped_tracks_60)
-    
+
 #     #remove duplicates from skipped_once
 #     skipped_once = user_data.skipped_once
 #     user_data.skipped_once = list(set(skipped_once))
 
 #     #not_skipped_tracks_60 are the songs found in played_tracks_60 that are found in recently_played
 #     not_skipped_tracks_60 = [track for track in played_tracks_60 if track in recently_played]
-    
+
 #     #remove songs from skipped_once if they are found in not_skipped_tracks_60
 #     user_data.skipped_once = [track for track in user_data.skipped_once if track not in not_skipped_tracks_60]
 
 #     #remove duplicates from skipped_twice
 #     skipped_twice = user_data.skipped_twice
 #     user_data.skipped_twice = list(set(skipped_twice))
-    
+
 #     print("Currently playing: " + str(currently_playing))
 #     # print("Played tracks 60: " + str(played_tracks_60))
 #     print("Skipped tracks 60: " + str(skipped_tracks_60))
--- api/models.py
+++ api/models.py
@@ -1,50 +1,57 @@
 from flask_sqlalchemy import SQLAlchemy
 from app_init import db
+
 # from app import app
 # db = SQLAlchemy(app, metadata=metadata)
 # # db.init_app(app)
 # migrate = Migrate(app, db)
 # migrate.init_app(app, db, render_as_batch=True)
 
+
 class Skipped(db.Model):
-  __tablename__ = 'skipped'
-  id = db.Column(db.Integer, primary_key=True)
-  skipped_count = db.Column(db.Integer)
-  track_id = db.Column(db.Integer, db.ForeignKey('track.id'))
-  playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'))
+    __tablename__ = "skipped"
+    id = db.Column(db.Integer, primary_key=True)
+    skipped_count = db.Column(db.Integer)
+    track_id = db.Column(db.Integer, db.ForeignKey("track.id"))
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.id"))
+
 
 class Track(db.Model):
-  __tablename__ = 'track'
-  id = db.Column(db.Integer, primary_key=True)
-  track_id = db.Column(db.String(120))
-  name = db.Column(db.String(120))
-  playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.id'))
+    __tablename__ = "track"
+    id = db.Column(db.Integer, primary_key=True)
+    track_id = db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    playlist_id = db.Column(db.Integer, db.ForeignKey("playlist.id"))
+
 
 class Playlist(db.Model):
-  __tablename__ = 'playlist'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(120))
-  playlist_id = db.Column(db.String(120))
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  currently_playing = db.Column(db.Boolean)
+    __tablename__ = "playlist"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    playlist_id = db.Column(db.String(120))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    currently_playing = db.Column(db.Boolean)
+
 
 class User(db.Model):
-  __tablename__ = 'user'
-  id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(120))
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120))
+
 
 class PrevQueue(db.Model):
-  __tablename__ = 'prev_queue'
-  id = db.Column(db.Integer, primary_key=True)
-  track_id = db.Column(db.Integer, db.ForeignKey('track.id'))
-  queue_index = db.Column(db.Integer)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    __tablename__ = "prev_queue"
+    id = db.Column(db.Integer, primary_key=True)
+    track_id = db.Column(db.Integer, db.ForeignKey("track.id"))
+    queue_index = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
 
 class OAuth(db.Model):
-  __tablename__ = 'oauth'
-  id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  access_token = db.Column(db.String(120))
-  refresh_token = db.Column(db.String(120))
-  expires_at = db.Column(db.Integer)
-  user = db.relationship('User', backref='oauth')
+    __tablename__ = "oauth"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    access_token = db.Column(db.String(120))
+    refresh_token = db.Column(db.String(120))
+    expires_at = db.Column(db.Integer)
+    user = db.relationship("User", backref="oauth")
