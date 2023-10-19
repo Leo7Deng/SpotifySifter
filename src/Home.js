70ec93155dd78cfe6647860c24defc3b7530c83b
Leo Deng <leo7deng@gmail.com>
2023-10-19 16:58:26 -0400

    home page

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
+import React, { useState } from 'react';
+import './Home.css';
+
 function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleLoginClick = async () => {
     try {
+      setIsLoading(true);
       const response = await fetch('http://localhost:8888/login', {
         method: 'GET',
       });
       if (response.status === 200) {
-        // Redirect to the Spotify authentication URL if the request was successful.
         const { auth_url } = await response.json();
         console.log(auth_url)
         window.location.href = auth_url;
@@ -19,11 +24,11 @@ function Home() {
 
   return (
     <div>
-      <h1>Spotify</h1>
+      <h1>Spotify Sifter</h1>
       <button onClick={handleLoginClick}>Login with Spotify</button>
+      {isLoading && <div className="loader"></div>}
     </div>
   );
-  }
+}
   
-  export default Home;
-  
+export default Home;
