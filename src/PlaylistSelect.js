30b4b61f8125dad6b75b3ff642333a095cbddb2a
Leo Deng <leo7deng@gmail.com>
2023-10-25 10:40:41 -0400

    block in center

--- src/PlaylistSelect.js
+++ src/PlaylistSelect.js
@@ -5,8 +5,25 @@ import React, { useState, useEffect } from 'react';
 function PlaylistSelect() {
     const [searchParams] = useSearchParams();
     const current_user_id = searchParams.get("current_user_id");
+    const [playlists, setPlaylists] = useState([]);
+    const [hoveredPlaylist, setHoveredPlaylist] = useState(null);
 
-    const [playlists, setPlaylists] = useState({});
+    // Preload iframe contents
+    useEffect(() => {
+        if (Array.isArray(playlists)) {
+            playlists.forEach(playlist => {
+                const iframe = document.createElement('iframe');
+                iframe.src = `https://open.spotify.com/embed/playlist/${playlist.id}?utm_source=generator`;
+                iframe.style.display = 'none';
+
+                iframe.onload = () => {
+                    document.body.removeChild(iframe);
+                };
+
+                document.body.appendChild(iframe);
+            });
+        }
+    }, [playlists]);
 
     useEffect(() => {
         fetch(`http://localhost:8888/get_playlists${current_user_id}`)
@@ -15,54 +32,43 @@ function PlaylistSelect() {
             .catch(error => console.error('Error:', error));
     }, [current_user_id]);
 
-    // const handleCheckboxChange = (playlistId) => {
-    //     // Toggle the selected status of the playlist
-    //     setSelectedPlaylists(prevState => {
-    //         if (prevState.includes(playlistId)) {
-    //             return prevState.filter(id => id !== playlistId);
-    //         } else {
-    //             return [...prevState, playlistId];
-    //         }
-    //     });
-    // };
-
-    // const handleSave = () => {
-    //     // Send a request to your backend to update the database
-    //     fetch('http://localhost:8888/update_database', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({ selectedPlaylists })
-    //     })
-    //     .then(response => {
-    //         if (response.ok) {
-    //             console.log('Data updated successfully');
-    //         } else {
-    //             console.error('Error updating data:', response.statusText);
-    //         }
-    //     })
-    //     .catch(error => console.error('Error:', error));
-    // };
-
     return (
         <div>
-            <div class="playlist-container">
-            {Array.isArray(playlists) && playlists.map(playlist => (
-                // <div key={playlist.id}>
-                <>
-                    {/* <input 
-                        class="playlist-checkbox"
-                        type="checkbox"
+            <div className="playlist-container">
+                {Array.isArray(playlists) && playlists.map((playlist) => (
+                    <div
                         key={playlist.id}
-                        // checked={selectedPlaylists.includes(playlist.id)}
-                        // onChange={() => handleCheckboxChange(playlist.id)}
-                    /> */}
-                    <iframe style={{ borderRadius: '12px' }} src={`https://open.spotify.com/embed/playlist/${playlist.id}?utm_source=generator`} width="100%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" class = "embed"></iframe>
-                </>
-                // </div>
-            ))}
+                        className="embed"
+                        onMouseEnter={() => setHoveredPlaylist(playlist.id)}
+                        onMouseLeave={() => setHoveredPlaylist(null)}
+                    >
+                        <iframe
+                            style={{ borderRadius: '12px' }}
+                            src={`https://open.spotify.com/embed/playlist/${playlist.id}?utm_source=generator`}
+                            width="100%"
+                            height="352"
+                            frameBorder="0"
+                            allowFullScreen=""
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"
+                        ></iframe>
+                    </div>
+                ))}
             </div>
+            {hoveredPlaylist !== null && (
+                <div className="large-card">
+                    <iframe
+                        style={{ borderRadius: '12px' }}
+                        src={`https://open.spotify.com/embed/playlist/${hoveredPlaylist}?utm_source=generator`}
+                        width="100%"
+                        height="352"
+                        frameBorder="0"
+                        allowFullScreen=""
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                        loading="lazy"
+                    ></iframe>
+                </div>
+            )}
         </div>
     );
 }
