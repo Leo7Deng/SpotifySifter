453ccd1cb2232cedfccbfe395bacad212eb75ee4
Leo Deng <leo7deng@gmail.com>
2024-01-28 17:08:37 -0500

    Create about page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -5,11 +5,17 @@ function About() {
     return (
         <>
             <div className="about">
-                <h1 className="about-title">About</h1>
-                <p className='about-text'>Spotify Sifter is a web app that allows you to sort out frequently skipped tracks from your playlists.</p>
-                <p className='about-text'>Spotify Sifter was created by Leo Deng.</p>
-                <p className='about-text'>Spotify Sifter is a personal project and is not affiliated with Spotify.</p>
-                <p className='about-text'>Spotify Sifter is open source. You can find the code on </p>
+                <p className='about-subtitle'>Spotify Sifter is a free service to remove frequently skipped tracks from your playlists.</p>
+                <p className='about-text'>A problem many Spotifer users face is having bloated playlists. Even good songs will be stuck in the wrong playlists, causing overskipping. To remedy this problem, Spotify Sifter allows users to automatically sift frequently skipped tracks into a "sorted" playlist which they can then review.</p>
+                <p className='about-subtitle'>How can I use it?</p>
+                <ol className='about-text'>
+                    <li>Log in with Spotify on our site.</li>
+                    <li>Select the playlists you want to sort.</li>
+                    <li>Close Spotify Sifter and start listening to music how you normally would.</li>
+                    <li>Periodically check back to see which songs have been sorted.</li>
+                </ol>
+                <p className='about-subtitle'>How does it work?</p>
+                <p className='about-text'>Spotify Sifter listens to a user's activity only when they are listening in a selected playlist. Looking at a user's queued songs periodically, we are able to figure out which songs are skipped. Shuffling and skipping to previous tracks will not disrupt our ability to view skipped tracks.</p>
             </div>
         </>
     );
