1786655e1fdb977e403835514ebe39a33d798a65
Leo Deng <leo7deng@gmail.com>
2024-02-01 11:58:20 -0500

    start of gear expand playlist item animation

--- frontend/src/PlaylistDisplay.js
+++ frontend/src/PlaylistDisplay.js
@@ -9,11 +9,13 @@ function PlaylistDisplay({ playlist, isChecked }) {
     const selectUrl = process.env.NODE_ENV === 'production' ? 'https://spotifysifter.up.railway.app/select' : 'http://localhost:8889/select';
     const unselectUrl = process.env.NODE_ENV === 'production' ? 'https://spotifysifter.up.railway.app/unselect' : 'http://localhost:8889/unselect';
     const [isAnimating, setIsAnimating] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
 
     const handleGearClick = () => {
         if (isAnimating) {
             return;
         }
+        setIsExpanded(!isExpanded);
         setGearChecked(!isGearChecked);
         setIsAnimating(true);
         setTimeout(() => {
@@ -37,37 +39,44 @@ function PlaylistDisplay({ playlist, isChecked }) {
     };
 
     return (
-        <div className="playlist-item">
-            <div className="info-wrapper">
-                <img
-                    className="playlist-image"
-                    src={playlist.image ? playlist.image : require('./images/LikedSongs.png')}
-                    alt="playlist"
-                />
-                <p className="playlist-name">{playlist.name}</p>
-            </div>
-            <div className="button-wrapper">
-                <button className={`gear${isGearChecked ? ' rotate' : ''}`} onClick={handleGearClick}>
-                    <img className="gear-icon" src={gearIcon} alt="gear" />
-                </button>
-                <div className="checkbox-wrapper-35">
-                    <input
-                        className="switch"
-                        type="checkbox"
-                        id={inputId}
-                        name={inputId}
-                        value="private"
-                        checked={checked}
-                        onChange={handleCheckboxChange}
+        <div className={`playlist-item ${isExpanded ? 'expanded' : 'collapsed'}`}>
+            <div className="playlist-info">
+                <div className="info-wrapper">
+                    <img
+                        className="playlist-image"
+                        src={playlist.image ? playlist.image : require('./images/LikedSongs.png')}
+                        alt="playlist"
                     />
-                    <label htmlFor={inputId}>
-                        <span className="switch-x-toggletext">
-                            <span className="switch-x-unchecked"><span className="switch-x-hiddenlabel">Unchecked: </span>Sift</span>
-                            <span className="switch-x-checked"><span className="switch-x-hiddenlabel">Checked: </span>Sifting</span>
-                        </span>
-                    </label>
+                    <p className="playlist-name">{playlist.name}</p>
+                </div>
+                <div className="button-wrapper">
+                    <button className={`gear${isGearChecked ? ' rotate' : ''}`} onClick={handleGearClick}>
+                        <img className="gear-icon" src={gearIcon} alt="gear" />
+                    </button>
+                    <div className="checkbox-wrapper-35">
+                        <input
+                            className="switch"
+                            type="checkbox"
+                            id={inputId}
+                            name={inputId}
+                            value="private"
+                            checked={checked}
+                            onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor={inputId}>
+                            <span className="switch-x-toggletext">
+                                <span className="switch-x-unchecked"><span className="switch-x-hiddenlabel">Unchecked: </span>Sift</span>
+                                <span className="switch-x-checked"><span className="switch-x-hiddenlabel">Checked: </span>Sifting</span>
+                            </span>
+                        </label>
+                    </div>
                 </div>
             </div>
+            {isExpanded && (
+                <div className={`expanded-content ${isExpanded ? 'expanded' : 'collapsed'}`}>
+                    <p className="expanded-description">blah blah</p>
+                </div>
+            )}
         </div>
     );
 }
