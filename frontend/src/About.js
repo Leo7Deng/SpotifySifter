c878dc0da053d52dd31dd88f9f0ce8eb8bd08bf6
Leo Deng <leo7deng@gmail.com>
2024-02-05 01:42:43 -0500

    add more info to about section

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -16,6 +16,12 @@ function About() {
                 </ol>
                 <p className='about-subtitle'>How does it work?</p>
                 <p className='about-text'>Spotify Sifter listens to a user's activity only when they are listening in a selected playlist. Looking at a user's queued songs periodically, we are able to figure out which songs are skipped. Shuffling and skipping to previous tracks will not disrupt our ability to view skipped tracks.</p>
+                <p className='about-subtitle'>Nothing is happening to my playlists, why?</p>
+                <p className='about-text'>Be patient and check back in a week. It takes time for Spotify Sifter to gather enough data to sort out the frequently skipped tracks.</p>
+                <p className='about-subtitle'>What should I do if I accidentally skip a song?</p>
+                <p className='about-text'>If you accidentally skip a song, don't worry. As long as you dont skip the same song too many times in a row, it will not be sorted out.</p>
+                <p className='about-subtitle'>Can I move the sorted songs back into the original playlist?</p>
+                <p className='about-text'>Yes, you can move the sorted songs back into the original playlist. This will not affect the sorting process. If you ever want to resort the playlist, you can click the resort button under a playlist's settings.</p>
                 <p className='about-subtitle'>What should I do if I deleted one of my sifted playlists?</p>
                 <p className='about-text'>If you accidentally delete a playlist that contains the sorted songs and you want it back, you can visit <a href="https://www.spotify.com/us/account/recover-playlists/">spotify.com/us/account/recover-playlists/</a> to restore it.</p>
             </div>
