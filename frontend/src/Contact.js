b978fc03028f2cd3042c838e8b578063fd1386cf
Leo Deng <leo7deng@gmail.com>
2024-02-27 21:14:48 -0500

    add personal website link in contact

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -9,8 +9,7 @@ function Contact() {
                 <a className="github" href="https://github.com/Leo7Deng/SpotifySifter">GitHub</a> <br></br>
                 Please feel free to contact me at&nbsp;
                 <a className="email" href="mailto:leo7deng@gmail.com">leo7deng@gmail.com</a>
-                . <br></br>You may also view my other projects here!
-                {/* <a href=" */}
+                . <br></br>You may also view my other projects <a href="https://leodeng.com">here</a>!
             </p>
 
         </div>
